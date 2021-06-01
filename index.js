062460a5a0697234057afc609c1d053f67b8937a
Tamim Badran <tamim22badran@gmail.com>
2021-06-01 17:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield", "Odie"];
-const prepend = ["Milo", "Otis", "Garfield"];
-const removeLast = ["Milo", "Otis", "Garfield"];
-const removeFirst = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield", "Odie"];
+const removeLast = ["Milo", "Otis", "Garfield", "Odie"];
+const removeFirst = ["Milo", "Otis", "Garfield", "Odie"];
