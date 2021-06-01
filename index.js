1ed97dd425e24768edb8228233bb755d4a84cd5b
Tamim Badran <tamim22badran@gmail.com>
2021-06-01 17:54:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield", "Odie"];
-const prepend = ["Milo", "Otis", "Garfield", "Odie"];
+const prepend = [ 'Odie', 'Milo', 'Otis', 'Garfield' ];
 const removeLast = ["Milo", "Otis", "Garfield", "Odie"];
 const removeFirst = ["Milo", "Otis", "Garfield", "Odie"];
